b377f7ef50d4a6ba95b1e0b83ba2d0ff4b06bcb4
Rasmus Knudsen <rasmuspriv@gmail.com>
2020-01-03 12:24:19 +0100

    More lobby work

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -1,26 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import styles from "./Lobby.module.css";
 
 const lobbyHost = "http://localhost:8000";
 
 export default function Lobby() {
+  const [cookies, setCookie] = useCookies(["playerName"]);
   const [matches, setMatches] = useState([]);
 
+  // On Mount
   useEffect(() => {
     fetchMatches();
-  });
+    const timer = setInterval(() => fetchMatches(), 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   async function fetchMatches() {
     const payload = await fetch(lobbyHost + "/games/TurnbasedGame");
-    console.log(await payload.json());
+    const json = await payload.json();
+    setMatches(json.rooms);
   }
 
-  async function createMatch() {}
+  async function createMatch() {
+    const payload = await fetch(lobbyHost + "/games/TurnbasedGame/create", {
+      method: "POST",
+      body: JSON.stringify({
+        numPlayers: 2
+      })
+    });
+    const json = await payload.json();
+    fetchMatches();
+  }
 
   return (
     <div className={styles.container}>
-      <div>Lobby</div>
-      <div>Matches:</div>
+      <h1>Lobby</h1>
+      <div>
+        <h2>Matches</h2>
+        <div className={styles.matchesContainer}>
+          {matches.map(m => (
+            <div key={m.gameID} className={styles.match}>
+              {m.gameID}
+            </div>
+          ))}
+        </div>
+      </div>
       <button onClick={createMatch}>Create Match</button>
     </div>
   );
