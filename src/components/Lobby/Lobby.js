9b1bda429ecd11bb3b8754c283f857dc1aa262ba
Rasmus <rasmuspriv@gmail.com>
2020-01-04 04:16:45 +0100

    Add version display

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -3,21 +3,31 @@ import { useCookies } from "react-cookie";
 import { withRouter } from "react-router";
 import styles from "./Lobby.module.css";
 
-const lobbyHost = `http://${window.location.hostname}/gameapi`;
+const IS_DEV = process.env.NODE_ENV === "development";
+
+const lobbyHost = IS_DEV ? `http://${window.location.hostname}:8000` : `http://${window.location.hostname}/gameapi`;
 
 export default withRouter(function Lobby({ match, location, history }) {
-  console.log(history);
   const [cookies, setCookie] = useCookies(["playerName", "myMatches"]);
   const [matches, setMatches] = useState([]);
+  const [version, setVersion] = useState("");
 
   const myMatches = cookies.myMatches ? cookies.myMatches : [];
 
   // On Mount
   useEffect(() => {
     fetchMatches();
+    fetchVersion();
     const timer = setInterval(() => fetchMatches(), 2000);
     return () => clearInterval(timer);
   }, []);
+
+  async function fetchVersion() {
+    const payload = await fetch(lobbyHost + "/version");
+    const json = await payload.json();
+    console.log(json);
+    setVersion(String(json.version));
+  }
 
   async function fetchMatches() {
     const payload = await fetch(lobbyHost + "/games/TurnbasedGame");
@@ -88,7 +98,10 @@ export default withRouter(function Lobby({ match, location, history }) {
 
   return (
     <div className={styles.container}>
-      <h1>Lobby</h1>
+      <div className={styles.header}>
+        <h1>Lobby</h1>
+        <h3>v. {version}</h3>
+      </div>
       <div>
         <h2>Matches</h2>
         <div className={styles.matchesContainer}>
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 import { Server } from "boardgame.io/server";
+import Router from "koa-router";
 import { TurnbasedGame } from "./gamelogic";
 
+const version = String(process.env.BUILD_NUMBER);
+const router = new Router();
 const server = Server({ games: [TurnbasedGame] });
+
+router.get("/version", (ctx, next) => {
+  ctx.res.setHeader("Access-Control-Allow-Origin", "*");
+  ctx.body = { version };
+});
+
+server.app.use(router.routes());
 server.run(8000);
