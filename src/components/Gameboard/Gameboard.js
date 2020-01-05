ac2e028907e98bb47710b30a30a41486a2c292bf
Rasmus <rasmuspriv@gmail.com>
2020-01-06 00:41:58 +0100

    Add water

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -128,6 +128,7 @@ export default withRouter(function Gameboard({ G, ctx, moves, events, isActive, 
 
   return (
     <div className={styles.backdrop}>
+      <div className={styles.ripple}></div>
       <h1
         className={cx(styles.turnLabel, {
           [styles.green]: isActive,
@@ -148,6 +149,20 @@ export default withRouter(function Gameboard({ G, ctx, moves, events, isActive, 
           <div className={styles.playerUnitsContainer}>{players?.map(renderPlayerUnit)}</div>
         </div>
       </div>
+      <svg>
+        <filter id="turbulence" x="0" y="0" width="100%" height="100%">
+          <feTurbulence id="sea-filter" numOctaves="1" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
+          <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+        </filter>
+        <animate
+          xlinkHref="#sea-filter"
+          attributeName="baseFrequency"
+          dur="60s"
+          keyTimes="0;0.5;1"
+          values="0.02 0.06;0.04 0.08;0.02 0.06"
+          repeatCount="indefinite"
+        />
+      </svg>
       {winner}
     </div>
   );
